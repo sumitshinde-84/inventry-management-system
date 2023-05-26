bc5600ab83ea147a18dd580c71ac682333a3b5e4
sumitshinde-84 <shindesumit217@gmail.com>
2023-05-26 13:29:09 +0530

    secure sesative information by dotenv

--- user-auth/app.js
+++ user-auth/app.js
@@ -3,71 +3,47 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-const mongoose = require("mongoose");
+var mongoose = require("mongoose")
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
-const catalogRouter = require("./routes/catalog");
-const compression = require("compression");
-const helmet = require("helmet");
-const RateLimit = require("express-rate-limit");
-const cors = require("cors"); 
-require('dotenv').config();
+
 var app = express();
 
-
+// view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
-app.use(express.static('public'));
+
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(compression());
-app.use(express.static(path.join(__dirname, "public")));
-app.use(
-  helmet.contentSecurityPolicy({
-    directives: {
-      "script-src": ["'self'", "code.jquery.com", "cdn.jsdelivr.net"],
-    },
-  })
-);
+app.use(express.static(path.join(__dirname, 'public')));
 
-const limiter = RateLimit({
-  windowMs: 1 * 60 * 1000,
-  max: 20,
-});
-
-app.use(limiter);
-app.use(cors()); 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
-app.use("/catalog", catalogRouter)
 
+// catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
 });
 
-
-
+// error handler
 app.use(function(err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-
+  // render the error page
   res.status(err.status || 500);
   res.render('error');
 });
 
 
-
-mongoose.set("strictQuery", false);
-const mongoDB = process.env.MONGODB_URI ;
-
+mongoose.set('strictQuery','false')
+const mongoDB= "mongodb+srv://Sumit:EilB6vWpXBsL8mXq@cluster0.uxibwot.mongodb.net/Authentication?retryWrites=true&w=majority";
 main().catch((err) => console.log(err));
 async function main() {
   await mongoose.connect(mongoDB);
 }
-
 
 module.exports = app;
---
+++ user-auth/model/user.js
@@ -0,0 +1,21 @@
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+const UserSchema = new Schema({
+  first_na: { type: String, required: true, maxLength: 100 },
+  family_name: { type: String, required: true, maxLength: 100 },
+  date_of_birth: { type: Date },
+  date_of_death: { type: Date },
+});
+
+;
+
+// Virtual for author's URL
+UserSchema.virtual("url").get(function () {
+  // We don't use an arrow function as we'll need the this object
+  return `/catalog/author/${this._id}`;
+});
+
+// Export model
+module.exports = mongoose.model("Author", UserSchema);
