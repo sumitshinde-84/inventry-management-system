6e6106b6e723a1ce55481980060bb6d6cce4c245
sumitshinde-84 <shindesumit217@gmail.com>
2023-05-31 11:56:04 +0530

    set user data to  session

--- app.js
+++ app.js
@@ -61,15 +61,7 @@ passport.deserializeUser(async function(id, done) {
     done(err);
   };
 });
-app.use( session({
-  secret: "your_session_secret",
-  resave: false,
-  saveUninitialized: false,
-  cookie: {
-    secure: true,
-    httpOnly: true, 
-  },
-}));
+app.use(session({ secret: "cats", resave: false, saveUninitialized: true }));
 app.use(passport.initialize());
 app.use(passport.session());
 
