f15e4312cbb0a63bfd675a7c8dff42f22775782b
sumitshinde-84 <shindesumit217@gmail.com>
2023-06-06 15:07:57 +0530

    fix login post

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,8 +1,8 @@
 const User = require("../model/user");
 const asyncHandler = require("express-async-handler");
 const { body, validationResult } = require("express-validator");
-const passport = require('passport');
-const bcrypt = require('bcryptjs');
+const passport = require("passport");
+const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 const crypto = require("crypto");
 
@@ -11,8 +11,14 @@ const secretKey = crypto.randomBytes(32).toString("hex");
 
 exports.register_user_post = [
   // Validate and sanitize request body fields
-  body("firstname").trim().notEmpty().withMessage("First name must be specified."),
-  body("lastname").trim().notEmpty().withMessage("Last name must be specified."),
+  body("firstname")
+    .trim()
+    .notEmpty()
+    .withMessage("First name must be specified."),
+  body("lastname")
+    .trim()
+    .notEmpty()
+    .withMessage("Last name must be specified."),
   body("email").trim().isEmail().withMessage("Invalid email address."),
   body("password").trim().notEmpty().withMessage("Password must be specified."),
 
@@ -37,10 +43,14 @@ exports.register_user_post = [
 
       await user.save();
 
-      const token = jwt.sign({ userId: user._id }, secretKey, { expiresIn: "1h" });
+      const token = jwt.sign({ userId: user._id }, secretKey, {
+        expiresIn: "1h",
+      });
 
       // Registration successful
-      return res.status(200).json({ message: "Registration successful", token });
+      return res
+        .status(200)
+        .json({ message: "Registration successful", token });
     } catch (err) {
       // Error occurred while registering
       console.error(err);
@@ -51,14 +61,30 @@ exports.register_user_post = [
   // Add a fallback response if the route is accessed directly
   (req, res) => {
     return res.status(404).json({ error: "Page not found" });
-  }
+  },
 ];
 
 exports.login_user_post = (req, res, next) => {
   passport.authenticate("local", { session: false }, (err, user, info) => {
     try {
-      if (err || !user) {
-        return res.status(401).json({ error: "Invalid credentials" });
+      if (err) {
+        // Error occurred
+        return res
+          .status(500)
+          .json({ error: "An error occurred during login" });
+      }
+
+      if (!user) {
+        // Authentication failed, check the info object for details
+        if (info && info.message === "Incorrect email") {
+          return res.status(401).json({ error: "Invalid email" });
+        }
+
+        if (info && info.message === "Incorrect password") {
+          return res.status(401).json({ error: "Invalid password" });
+        }
+
+        return res.status(401).json({ error: "Invalid email or password" });
       }
 
       req.login(user, { session: false }, (err) => {
@@ -66,7 +92,9 @@ exports.login_user_post = (req, res, next) => {
           throw err;
         }
 
-        const token = jwt.sign({ userId: user._id }, secretKey, { expiresIn: "1h" });
+        const token = jwt.sign({ userId: user._id }, secretKey, {
+          expiresIn: "1h",
+        });
 
         return res.status(200).json({ message: "Login successful", token });
       });
@@ -78,19 +106,19 @@ exports.login_user_post = (req, res, next) => {
 };
 
 exports.user_list = asyncHandler(async (req, res, next) => {
-  const allUsers = await User.find({}).exec()
+  const allUsers = await User.find({}).exec();
 
   if (allUsers === null) {
-    const err = new Error('Users not found');
+    const err = new Error("Users not found");
     err.status = 404;
     return next(err);
   }
 
   const responseData = {
-    title: 'Users',
-    content: 'user_list',
-    users: allUsers
+    title: "Users",
+    content: "user_list",
+    users: allUsers,
   };
 
-  res.render('layout', responseData);
+  res.render("layout", responseData);
 });
