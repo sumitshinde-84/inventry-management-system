5de7ee719c25430d3bf5e2b247dd2bd7193f0b3c
sumitshinde-84 <shindesumit217@gmail.com>
2023-06-06 15:16:33 +0530

    fix login post

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
-var createError = require("http-errors");
-var express = require("express");
-var path = require("path");
-var cookieParser = require("cookie-parser");
-var logger = require("morgan");
+var createError = require('http-errors');
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
 const mongoose = require("mongoose");
-var indexRouter = require("./routes/index");
-var usersRouter = require("./routes/users");
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
 const catalogRouter = require("./routes/catalog");
 const session = require("express-session");
 const passport = require("passport");
@@ -13,53 +13,58 @@ const LocalStrategy = require("passport-local").Strategy;
 const compression = require("compression");
 const helmet = require("helmet");
 const RateLimit = require("express-rate-limit");
-const cors = require("cors");
-const bcrypt = require("bcryptjs");
-require("dotenv").config();
-const User = require("./model/user");
+const cors = require("cors"); 
+const bcrypt = require('bcryptjs')
+require('dotenv').config();
+const User = require('./model/user')
 var app = express();
 
-app.set("views", path.join(__dirname, "views"));
-app.set("view engine", "ejs");
-app.use(express.static("public"));
-app.use(logger("dev"));
+
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+app.use(express.static('public'));
+app.use(logger('dev'));
 app.use(express.json());
 
 passport.use(
-  new LocalStrategy(async (email, password, done) => {
+  new LocalStrategy(async(email, password, done) => {
     try {
       const user = await User.findOne({ email: email });
       if (!user) {
         return done(null, false, { message: "Incorrect email" });
-      }
-
-      const passwordMatch = await bcrypt.compare(password, user.password);
-      if (passwordMatch) {
-        return done(null, user);
-      } else {
-        return done(null, false, { message: "Incorrect password" });
-      }
-    } catch (err) {
+      };
+      bcrypt.compare(password, user.password, (err, res) => {
+        if (res) {
+          // passwords match! log user in
+          return done(null, user)
+        } else {
+          // passwords do not match!
+          return done(null, false, { message: "Incorrect password" })
+        }
+      })
+      return done(null, user);
+    } catch(err) {
       return done(err);
-    }
+    };
   })
 );
 
-passport.serializeUser(function (user, done) {
+passport.serializeUser(function(user, done) {
   done(null, user.id);
 });
 
-passport.deserializeUser(async function (id, done) {
+passport.deserializeUser(async function(id, done) {
   try {
     const user = await User.findById(id);
     done(null, user);
-  } catch (err) {
+  } catch(err) {
     done(err);
-  }
+  };
 });
 app.use(session({ secret: "cats", resave: false, saveUninitialized: true }));
 app.use(passport.initialize());
 app.use(passport.session());
+
 
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
@@ -79,23 +84,28 @@ const limiter = RateLimit({
 });
 
 app.use(limiter);
-app.use(cors());
-app.use("/", indexRouter);
-app.use("/users", usersRouter);
-app.use("/catalog", catalogRouter);
+app.use(cors()); 
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+app.use("/catalog", catalogRouter)
 
-app.use(function (req, res, next) {
+app.use(function(req, res, next) {
   next(createError(404));
 });
 
-app.use(function (err, req, res, next) {
+
+
+app.use(function(err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
-  res.locals.error = req.app.get("env") === "development" ? err : {};
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
 
   res.status(err.status || 500);
-  res.render("error");
+  res.render('error');
 });
+
+
 
 mongoose.set("strictQuery", false);
 const mongoDB = process.env.MONGODB_URI;
@@ -104,5 +114,6 @@ main().catch((err) => console.log(err));
 async function main() {
   await mongoose.connect(mongoDB);
 }
+
 
 module.exports = app;
